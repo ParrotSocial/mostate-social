ce7d1854239918706f22b675cd0dc7016a8a9bf6
Cole R Lawrence <Colelawr@gmail.com>
2016-09-05 17:23:04 -0500

    Open corresponding action urls passed from previous app

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -2,4 +2,28 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { Globals } from './app/globals'
+
+// action url provided
+const ActionURLs = {
+  "submit-event": Globals.SUBMISSION_URL,
+  "submit-feedback": Globals.FEEDBACK_URL
+}
+
+// check if this URL is trying to redirect to a url
+const searchOpenURLMatch = /action=([\w\-]+)/.exec(location.search)
+let markForNavigation: boolean = false
+if (searchOpenURLMatch) {
+  const URLKey = searchOpenURLMatch[1]
+  const URL = ActionURLs[URLKey]
+  if (!URL) {
+    console.warn(`Url not found for action: ${URLKey}`)
+  } else {
+    location.href = URL
+    markForNavigation = true
+  }
+}
+
+if (!markForNavigation) {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}
