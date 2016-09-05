fceb492e0eb9b27fb8b5635661f648e8d9de20a5
Cole R Lawrence <Colelawr@gmail.com>
2016-09-05 15:47:55 -0500

    Create globals links for forms

---
+++ src/app/globals.ts
@@ -0,0 +1,5 @@
+
+export const Globals = {
+  SUBMISSION_URL: "https://docs.google.com/forms/d/e/1FAIpQLSfUeY-XU-FUfP9ulh5eTVWksAZbtHFnr_JMTRP84Q8MgXf5aw/viewform",
+  FEEDBACK_URL: "https://docs.google.com/a/parrot.social/forms/d/e/1FAIpQLSeqYFBincbnVe4ejt_wd8r1iuHcFXfl5hmr_MfCSbdG9jZdLQ/viewform"
+}
