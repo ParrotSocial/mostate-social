101c43cc976620694a7d40eb924e0cf4aad98e38
Cole R Lawrence <Colelawr@gmail.com>
2016-09-05 19:01:25 -0500

    Custom emoji name flag

--- src/app/emoji.list.ts
+++ src/app/emoji.list.ts
@@ -1,6 +1,10 @@
 var unicode = require('emoji-unicode-map')
 
 export let emojiByName: {[name: string]: string} = {}
+
+// custom emoji replacements
+unicode.emoji['🚩'] = 'flag'
+
 for (let codePoint in unicode.emoji) {
   const name = unicode.emoji[codePoint]
   emojiByName[name] = codePoint
